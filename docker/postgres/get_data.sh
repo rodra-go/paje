8133c54097eff63f0a6d93231d57ef857475399c
unknown <Rodrigo Cunha>
2019-10-08 13:35:04 -0300

    Postres database configuration

--- docker/postgres/get_data.sh
+++ docker/postgres/get_data.sh
@@ -1,6 +1,1 @@
 #!/bin/bash
-wget -P /tmp/ https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip
-sudo unzip /tmp/household_power_consumption.zip
-# rm -rf /tmp/*.zip
-# sed '/^\s*$/d' /tmp/household_power_consumption.txt > /tmp/household_power_consumption.csv
-# rm -rf /tmp/*.txt
